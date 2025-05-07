79224a7469b16556becbf073404739adb78298ff
Md. Abdur Rahim <raahimm11@gmail.com>
2025-05-07 22:12:12 +0600

    fixed code formatting.

--- public/script.js
+++ public/script.js
@@ -29,17 +29,14 @@ msgerForm.addEventListener("submit", event => {
 });
 
 function appendMessage(name, img, side, text) {
-  //   Simple solution for small apps
   const msgHTML = `
     <div class="msg ${side}-msg">
       <div class="msg-img" style="background-image: url(${img})"></div>
-
       <div class="msg-bubble">
         <div class="msg-info">
           <div class="msg-info-name">${name}</div>
           <div class="msg-info-time">${formatDate(new Date())}</div>
         </div>
-
         <div class="msg-text">${text}</div>
       </div>
     </div>
